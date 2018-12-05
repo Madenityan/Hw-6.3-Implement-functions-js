2bc04c8fb93c3ecc777c771aedc44c940d412c03
Madenityan <marinakorsyn@gmail.com>
2018-12-05 09:30:37 +0200

    Implement functions (reverse)

--- js/main.js
+++ js/main.js
@@ -86,11 +86,12 @@ function reverse(str) {
     }
     return reversed;
 }
-console.log(reverse('abcdef'));
+console.log(reverse(''));
 
 // 6. //indexOf - Implement the indexOf function for arrays.
 // indexOf([1, 2, 3], 1)               // 0
 // indexOf([1, 2, 3], 4)               // -1
+
 
 
 
